f8a33b653e3ab66de9f83f6009e0e15211a8fba3
AMSOU ISMAIL <143527851+amsou-ismail@users.noreply.github.com>
2025-04-02 23:48:54 +0000

    Update scanner.py
    network scanner

--- scanner.py
+++ scanner.py
@@ -2,9 +2,10 @@ import sys
 import socket
 
 from tabulate import tabulate # pour affichage self.macList
+# sudo apt install python3-tabulate
 # python -m pip install tabulate
 from scapy.all import Ether, ARP, srp
-# apt install python3-scapy
+# sudo apt install python3-scapy
 # python -m pip install scapy
 
 class Port_scanner :
@@ -29,24 +30,31 @@ class Port_scanner :
             if result == 0: 
                 self.port_ouvert.append(port) 
 
-class Network_scanner :
-    def __init__(self,address_resau = "192.168.X.X/24",interface = "eth0"):
+# target = Port_scanner("10.10.53.128")
+# target.port_scan()
+# print(target.port_ouvert)
+
+class Network_scanner:
+    def __init__(self, address_resau="192.168.100.0/24", interface="eth0"):
         self.target = address_resau
         self.interface = interface
         self.broadcastMac = "ff:ff:ff:ff:ff:ff"
         self.macList = []
-    
+
     def scan_network(self):
         packet = Ether(dst=self.broadcastMac)/ARP(pdst=self.target)
         ans, _ = srp(packet, timeout=2, iface=self.interface, inter=0.1, verbose=False)
         self.macList = [(receive.psrc, receive.src) for _, receive in ans]
-        
-        # print(tabulate(results, headers=["Adresse IP", "Adresse MAC"], tablefmt="grid"))        
+
+    def mac_table(self):
+        print(tabulate(self.macList, headers=["Adresse IP", "Adresse MAC"], tablefmt="grid"))
+
+# n = Network_scanner()
+# n.scan_network()
+# n.mac_table()
+
 
 # n = Network_scanner("10.10.10.10","m")
 # print(n.target,n.interface)
     
 
-# target = Port_scanner("10.10.53.128")
-# target.port_scan()
-# print(target.port_ouvert)
